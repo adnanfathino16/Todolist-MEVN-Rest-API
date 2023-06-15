9688af7cac9366d083a960608e697996892f9239
adnanfathino16 <fathinoadnan@gmail.com>
2023-06-15 13:57:03 +0700

    update db config

--- moexpress-server/config/db.config.js
+++ moexpress-server/config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  url: "mongodb+srv://mern-vercel:fuyYoMalNzKjYFNM@cluster0.ddoahki.mongodb.net/?retryWrites=true&w=majority",
+  url: "mongodb+srv://mern-vercel:fuyYoMalNzKjYFNM@cluster0.ddoahki.mongodb.net/",
 };
